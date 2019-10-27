7de88c50a950fa3bc00f017a18474767042f6442
amhossain93 <a00hossain@gmail.com>
2019-10-27 22:39:26 +0600

    Delecting categories from admin panel

---
+++ 13. Categories & More/102.delecting-categories.php
@@ -0,0 +1,93 @@
+<?php include "includes/header.php";?>
+    <div id="wrapper">
+        <?php include "includes/navigation.php";?>
+        <div id="page-wrapper">
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Blank Page
+                            <small>Subheading</small>
+                        </h1>
+                    </div>
+                    <div class="col-sm-6">
+                       <?php
+                        if(isset($_POST['submit'])){
+                            $cat_title = $_POST['cat_title'];
+                            
+                            if($cat_title == "" || empty($cat_title)){
+                                echo "Field should not be empty";
+                            }else{
+                                $query = "INSERT INTO categories(cat_title)";
+                                $query .= "VALUE('{$cat_title}')";
+                                
+                                $create_category_query = mysqli_query($connection, $query);
+                                
+                                if(!$create_category_query){
+                                    die('QUERY_FAILED' . mysql_error($connection));
+                                }
+                            }
+                        }
+                        ?>
+                       
+                       
+                        <form action="" method="post">
+                            <div class="form-group">
+                                <label for="cat_title">Add Category:</label>
+                                <input type="text" class="form-control" name="cat_title" id="pwd">
+                            </div>
+                            <button type="submit" class="btn btn-primary" name="submit">Add Categories</button>
+                        </form>
+                    </div>
+                    <div class="col-sm-6">
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Category Title</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                    // Select all data from categoried
+                                    $query = "SELECT * FROM categories";
+                                    // Connect data for getting data from categories
+                                    $select_categories = mysqli_query($connection, $query);
+
+                                    // Fetch the category from categories table by associative array
+                                    while ($row = mysqli_fetch_assoc($select_categories)) 
+                                    {
+                                        $cat_id = $row["cat_id"];
+                                        $cat_title = $row["cat_title"];
+
+                                        echo "<tr>";
+                                        echo "<td>{$cat_id}</td>";
+                                        echo "<td>{$cat_title}</td>";
+                                        echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
+                                        echo "</tr>";
+                                    }
+                                
+                                    if(isset($_GET['delete'])){
+                                        $the_cat_id = $_GET['delete'];
+                                        $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id}";
+                                        $delete_query = mysqli_query($connection, $query);
+                                        
+                                        header("Location: categories.php");
+                                    }
+
+                                ?> 
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    <?php include "includes/footer.php";?>
