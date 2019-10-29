725b1d95b42c6e8c7cef3f3e6596a0cf04ad8da0
amhossain93 <a00hossain@gmail.com>
2019-10-29 23:38:37 +0600

    Create posts.php file in admin panel to post dynamically

---
+++ admin/posts.php
@@ -0,0 +1,55 @@
+<?php include "includes/header.php";?>
+<div id="wrapper">
+    <?php include "includes/navigation.php";?>
+    <div id="page-wrapper">
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Blank Page
+                        <small>Subheading</small>
+                    </h1>
+                </div>
+
+                <div class="col-sm-6">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Author</th>
+                                <th>Title</th>
+                                <th>Category</th>
+                                <th>Status</th>
+                                <th>Image</th>
+                                <th>Tags</th>
+                                <th>Comments</th>
+                                <th>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>10</td>
+                                <td>Jhon Doe</td>
+                                <td>Bootstrap Framework</td>
+                                <td>Bootstrap</td>
+                                <td>Status</td>
+                                <td>Image</td>
+                                <td>Tags</td>
+                                <td>Comments</td>
+                                <td>Date</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+
+    <?php include "includes/footer.php";?>
